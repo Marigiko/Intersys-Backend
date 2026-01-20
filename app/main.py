1f850a6baa6b430e7401a2162fb3aee93e337260
KeyzDev <marioaquinojob@gmail.com>
2026-01-20 20:56:51 -0300

    middleware add

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
 from app.api.v1.router import api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Intersys API",
     version="1.0.0",
     description="API for Intersys application",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router, prefix="/api/v1")
